0ee59b43a8244414149cdadb2c8da82f6e964ead
Oumar Cissé <cisseoumar2025@gmail.com>
2026-08-16 14:25:10 +0200

    web: function CvDocumentSidebar({ cvReview, copy, avatarDataUrl }) {

--- career-web-react/src/App.jsx
+++ career-web-react/src/App.jsx
@@ -13731,7 +13731,7 @@ function CvDocumentSidebar({ cvReview, copy, avatarDataUrl }) {
               <div>
                 <h4>{copy.cvPreviewTechnicalSkills}</h4>
                 <div className="cv-document-skill-chips">
-                  {cvReview.skills.map((skill) => (
+                  {cvReview.skills.filter(Boolean).map((skill) => (
                     <span key={skill} className="cv-document-skill-chip">
                       {skill}
                     </span>
